80e88474a8511dfee463708894e48b5e6bf412b9
Shishir Gnawali <gnawalishishir53@gmail.com>
2026-08-09 18:53:09 +0545

    Updated config.py for localhost

--- config.py
+++ config.py
@@ -3,12 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-secret')
+    SECRET_KEY = os.getenv("SECRET_KEY")
 
-    MYSQL_HOST = os.environ.get('MYSQL_HOST')
-    MYSQL_USER = os.environ.get('MYSQL_USER')
-    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD')
-    MYSQL_DB = os.environ.get('MYSQL_DB')
-    MYSQL_PORT = int(os.environ.get('MYSQL_PORT', 3306))
+    MYSQL_HOST = os.getenv("MYSQL_HOST")
+    MYSQL_USER = os.getenv("MYSQL_USER")
+    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+    MYSQL_DB = os.getenv("MYSQL_DB")
